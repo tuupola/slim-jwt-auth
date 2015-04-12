9304677e943cae8e406994e21c5710cc3a065fbb
Mika Tuupola <tuupola@appelsiini.net>
2015-04-12 15:46:58 +0700

    Add tests for all missing methods

--- tests/JwtAuthenticationTest.php
+++ tests/JwtAuthenticationTest.php
@@ -93,6 +93,36 @@ class JwtBasicAuthenticationTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals("Foo", $app->response()->body());
     }
 
+    public function testShouldReturn401WithFalseFromCallback()
+    {
+        \Slim\Environment::mock(array(
+            "SCRIPT_NAME" => "/index.php",
+            "PATH_INFO" => "/api/foo",
+            "HTTP_AUTHORIZATION" => "Bearer " . self::$token
+        ));
+        $app = new \Slim\Slim();
+        $app->get("/foo/bar", function () {
+            echo "Success";
+        });
+        $app->get("/api/foo", function () {
+            echo "Foo";
+        });
+
+        $auth = new \Slim\Middleware\JwtAuthentication(array(
+            "secret" => "supersecretkeyyoushouldnotcommittogithub",
+            "callback" => function ($decoded, $app) {
+                return false;
+            }
+        ));
+
+        $auth->setApplication($app);
+        $auth->setNextMiddleware($app);
+        $auth->call();
+
+        $this->assertEquals(401, $app->response()->status());
+        $this->assertEquals("", $app->response()->body());
+    }
+
     public function testShouldReturn200WithOptions()
     {
         \Slim\Environment::mock(array(
@@ -174,6 +204,23 @@ class JwtBasicAuthenticationTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals("Success", $app->response()->body());
     }
 
+    public function testShouldFetchTokenFromEnvironment()
+    {
+        \Slim\Environment::mock(array(
+            "SCRIPT_NAME" => "/index.php",
+            "PATH_INFO" => "/public/foo"
+        ));
+
+        $_SERVER["HTTP_BRAWNDO"] = "Bearer " . self::$token;
+
+        $auth = new \Slim\Middleware\JwtAuthentication(array(
+            "environment" => "HTTP_BRAWNDO",
+            "secret" => "supersecretkeyyoushouldnotcommittogithub"
+        ));
+
+        $this->assertEquals(self::$token, $auth->fetchToken());
+    }
+
     public function testShouldCallCallback()
     {
         \Slim\Environment::mock(array(
@@ -236,5 +283,49 @@ class JwtBasicAuthenticationTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals("Foo", $app->response()->body());
         $this->assertTrue(is_object($app->jwt));
         $this->assertTrue(in_array("delete", $app->jwt->scope));
+    }
+
+    public function testShouldGetAndSetPath()
+    {
+        $auth = new \Slim\Middleware\JwtAuthentication;
+        $auth->setPath("/admin");
+        $this->assertEquals("/admin", $auth->getPath());
+    }
+
+    public function testShouldGetAndSetSecret()
+    {
+        $auth = new \Slim\Middleware\JwtAuthentication;
+        $auth->setSecret("supersecretkeyyoushouldnotcommittogithub");
+        $this->assertEquals("supersecretkeyyoushouldnotcommittogithub", $auth->getSecret());
+    }
+
+    public function testShouldGetAndSetEnvironment()
+    {
+        $auth = new \Slim\Middleware\JwtAuthentication;
+        $auth->setEnvironment("HTTP_SOMETHING");
+        $this->assertEquals("HTTP_SOMETHING", $auth->getEnvironment());
+    }
+
+    public function testShouldGetAndSetCallback()
+    {
+        $auth = new \Slim\Middleware\JwtAuthentication;
+        $auth->setCallback(function ($decoded, $app) {
+            return true;
+        });
+        $this->assertTrue(is_callable($auth->getCallback()));
+    }
+
+    public function testShouldGetAndSetRules()
+    {
+        $auth = new \Slim\Middleware\JwtAuthentication;
+        $auth->setRules([
+            function ($app) {
+                return true;
+            },
+            function ($app) {
+                return false;
+            }
+        ]);
+        $this->assertEquals(2, count($auth->getRules()));
     }
 }
