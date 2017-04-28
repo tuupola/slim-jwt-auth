874d448f1c9daa083112c481af6a7aa6154603e4
Mika Tuupola <tuupola@appelsiini.net>
2017-04-28 14:06:49 +0700

    Add missing files

---
+++ src/JwtAuthentication/CallableDelegate.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * This file is part of PSR-7 JSON Web Token Authentication middleware
+ *
+ * Copyright (c) 2015-2016 Mika Tuupola
+ *
+ * Licensed under the MIT license:
+ *   http://www.opensource.org/licenses/mit-license.php
+ *
+ * Project home:
+ *   https://github.com/tuupola/slim-jwt-auth
+ *
+ */
+
+namespace Tuupola\Middleware\JwtAuthentication;
+
+use Interop\Http\ServerMiddleware\DelegateInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
+final class CallableDelegate implements DelegateInterface
+{
+    private $callable;
+    private $response;
+
+    public function __construct(callable $callable, ResponseInterface $response)
+    {
+        $this->callable = $callable;
+        $this->response = $response;
+    }
+
+    public function process(ServerRequestInterface $request)
+    {
+        $callable = $this->callable;
+        return $callable($request, $this->response);
+    }
+}
---
+++ src/JwtAuthentication/ResponseFactory.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * This file is part of PSR-7 JSON Web Token Authentication middleware
+ *
+ * Copyright (c) 2015-2016 Mika Tuupola
+ *
+ * Licensed under the MIT license:
+ *   http://www.opensource.org/licenses/mit-license.php
+ *
+ * Project home:
+ *   https://github.com/tuupola/slim-jwt-auth
+ *
+ */
+
+namespace Tuupola\Middleware\JwtAuthentication;
+
+use Slim\Http\Response as SlimResponse;
+use Zend\Diactoros\Response as DiactorosResponse;
+use GuzzleHttp\Psr7\Response as GuzzleResponse;
+use Nyholm\Psr7\Response as NyholmResponse;
+use Interop\Http\Factory\ResponseFactoryInterface;
+
+final class ResponseFactory implements ResponseFactoryInterface
+{
+    public function createResponse($code = 200)
+    {
+        if (class_exists(SlimResponse::class)) {
+            return new SlimResponse($code);
+        }
+
+        if (class_exists(DiactorosResponse::class)) {
+            return new DiactorosResponse("php://memory", $code);
+        }
+
+        if (class_exists(GuzzleResponse::class)) {
+            return new GuzzleResponse($code);
+        }
+
+        if (class_exists(NyholmResponse::class)) {
+            return new NyholmResponse($code);
+        }
+
+        throw new \RuntimeException("No PSR-7 implementation available");
+    }
+}
