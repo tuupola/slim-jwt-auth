8e40f2b8f7c4deab307ff29631d4caf461a8adc9
Mika Tuupola <tuupola@appelsiini.net>
2018-03-02 13:32:57 +0700

    Hello 2018

--- src/JwtAuthentication/RuleInterface.php
+++ src/JwtAuthentication/RuleInterface.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 /**
  * This file is part of PSR-7 & PSR-15 JWT Authentication middleware
  *
- * Copyright (c) 2015-2017 Mika Tuupola
+ * Copyright (c) 2015-2018 Mika Tuupola
  *
  * Licensed under the MIT license:
  *   http://www.opensource.org/licenses/mit-license.php
