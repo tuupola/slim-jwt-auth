4b8a6ab9afd75ec6b0c88d8e4f2525f844f67053
TiMESPLiNTER <github@timesplinter.ch>
2020-04-05 09:37:04 +0200

    Respect X-Forwarded-Proto header for relaxation

--- src/JwtAuthentication.php
+++ src/JwtAuthentication.php
@@ -115,7 +115,8 @@ final class JwtAuthentication implements MiddlewareInterface
      */
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
-        $scheme = $request->getUri()->getScheme();
+        $forwardProto = $request->getHeaderLine('X-Forwarded-Proto');
+        $scheme =  '' !== $forwardProto ? $forwardProto : $request->getUri()->getScheme();
         $host = $request->getUri()->getHost();
 
         /* If rules say we should not authenticate call next and return. */
@@ -127,8 +128,9 @@ final class JwtAuthentication implements MiddlewareInterface
         if ("https" !== $scheme && true === $this->options["secure"]) {
             if (!in_array($host, $this->options["relaxed"])) {
                 $message = sprintf(
-                    "Insecure use of middleware over %s denied by configuration.",
-                    strtoupper($scheme)
+                    "Insecure use of middleware over %s for host %s denied by configuration.",
+                    strtoupper($scheme),
+                    $host
                 );
                 throw new RuntimeException($message);
             }
