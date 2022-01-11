0a829fe934c5edfd9794165e44d906c3faf1bdb0
Mika Tuupola <tuupola@appelsiini.net>
2022-01-11 15:49:37 +0200

    Hello 2022!

--- src/JwtAuthentication/RequestPathRule.php
+++ src/JwtAuthentication/RequestPathRule.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 /*
 
-Copyright (c) 2015-2021 Mika Tuupola
+Copyright (c) 2015-2022 Mika Tuupola
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
